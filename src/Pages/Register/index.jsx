f8a573ae262a323ffe6532bf7e89e8e1bcff5be6
Filipelevar <filipelevar@icloud.com>
2023-07-02 02:11:34 -0300

    feature: validation Email

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -1,5 +1,233 @@
+// import { Link } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import { LayoutComponents } from "../../components/LayoutComponents";
+// import InputMask from "react-input-mask";
+
+// export const Register = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [name, setName] = useState("");
+//   const [Cep, setCep] = useState("");
+//   const [DataNascimento, setDataNascimento] = useState("");
+
+//   function validateEmail(email) {
+//     const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+//     if (email !== "" && email.match(regex)) {
+//       setEmail(email);
+//     }
+//     return false;
+//   }
+
+//   // useEffect(() => {
+//   //   fetch('http://localhost:5000').then((data) => {
+//   //     return data.json()
+//   //   })
+//   //     .then((data) => {
+//   //       console.log(data)
+//   //     })
+//   // }, [])
+
+//   useEffect(() => {
+//     fetch("http://localhost:5000")
+//       .then((data) => data.json())
+//       .then((data) => console.log(data));
+//   }, []);
+
+//   return (
+//     <LayoutComponents>
+//       <form className="login-form">
+//         <span className="login-form-title"> Criar Conta </span>
+
+//         <span className="login-form-title"></span>
+
+//         <div className="wrap-input">
+//           <input
+//             className={name !== "" ? "has-val input" : "input"}
+//             type="email"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//           />
+//           <span className="focus-input" data-placeholder="Nome Completo"></span>
+//         </div>
+
+//         <div className="wrap-input">
+//           <input
+//             className={email !== "" ? "has-val input" : "input"}
+//             type="email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//           />
+//           <span className="focus-input" data-placeholder="Email"></span>
+//         </div>
+
+//         <div className="wrap-input">
+//           <input
+//             className={password !== "" ? "has-val input" : "input"}
+//             type="password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//           <span className="focus-input" data-placeholder="Password"></span>
+//         </div>
+//         <div className="wrap-input">
+//           <InputMask
+//             id="input-mask"
+//             mask="99999-999"
+//             className={Cep !== "" ? "has-val input" : "input"}
+//             value={Cep}
+//             onChange={(e) => setCep(e.target.value)}
+//           ></InputMask>
+//           <span
+//             className="focus-input"
+//             data-placeholder="Digite seu Cep"
+//           ></span>
+//         </div>
+//         <div className="wrap-input">
+//           <InputMask
+//             id="input-mask"
+//             mask="99/99/9999"
+//             className={DataNascimento !== "" ? "has-val input" : "input"}
+//             value={DataNascimento}
+//             onChange={(e) => setDataNascimento(e.target.value)}
+//           ></InputMask>
+//           <span
+//             className="focus-input"
+//             data-placeholder="Data de Nascimento"
+//           ></span>
+//         </div>
+
+//         <div className="container-login-form-btn">
+//           <button className="login-form-btn">Register</button>
+//         </div>
+
+//         <div className="text-center">
+//           <span className="txt1">Já possui conta? </span>
+//           <Link className="txt2" to="/">
+//             Acessar com Email e Senha.
+//           </Link>
+//         </div>
+//         <div></div>
+//       </form>
+//     </LayoutComponents>
+//   );
+// };
+
+// import { Link } from "react-router-dom";
+// import { useEffect, useState, useRef } from "react";
+// import { LayoutComponents } from "../../components/LayoutComponents";
+// import InputMask from "react-input-mask";
+
+// export const Register = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [name, setName] = useState("");
+//   const [Cep, setCep] = useState("");
+//   const [DataNascimento, setDataNascimento] = useState("");
+//   const inputEmail = useRef(null);
+//   const isFocused = inputEmail.current === document.activeElement;
+//   const [bolEmail, setBolEmail] = useState(false);
+
+//   const validateEmail = (email) => {
+//     const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+//     return email !== "" && email.match(regex) ? true : false;
+//   };
+
+//   useEffect(() => {
+//     setBolEmail(validateEmail(email));
+//     console.log("focus", isFocused);
+//   }, [email, isFocused]);
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//   };
+
+//   return (
+//     <LayoutComponents>
+//       <form action="submit" className="login-form" onSubmit={handleSubmit}>
+//         <span className="login-form-title"> Criar Conta </span>
+
+//         <span className="login-form-title"></span>
+
+//         <div className="wrap-input">
+//           <input
+//             className={name !== "" ? "has-val input" : "input"}
+//             type="text"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//             required
+//           />
+//           <span className="focus-input" data-placeholder="Nome Completo"></span>
+//         </div>
+
+//         <div className="wrap-input">
+//           <input
+//             ref={inputEmail}
+//             className={email !== "" ? "has-val input" : "input"}
+//             type="email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             required
+//           />
+//           <span className="focus-input" data-placeholder="Email"></span>
+//           {isFocused && !bolEmail && <span>email inválido</span>}
+//         </div>
+
+//         <div className="wrap-input">
+//           <input
+//             className={password !== "" ? "has-val input" : "input"}
+//             type="password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//           <span className="focus-input" data-placeholder="Password"></span>
+//         </div>
+//         <div className="wrap-input">
+//           <InputMask
+//             id="input-mask"
+//             mask="99999-999"
+//             className={Cep !== "" ? "has-val input" : "input"}
+//             value={Cep}
+//             onChange={(e) => setCep(e.target.value)}
+//           ></InputMask>
+//           <span
+//             className="focus-input"
+//             data-placeholder="Digite seu Cep"
+//           ></span>
+//         </div>
+//         <div className="wrap-input">
+//           <InputMask
+//             id="input-mask"
+//             mask="99/99/9999"
+//             className={DataNascimento !== "" ? "has-val input" : "input"}
+//             value={DataNascimento}
+//             onChange={(e) => setDataNascimento(e.target.value)}
+//           ></InputMask>
+//           <span
+//             className="focus-input"
+//             data-placeholder="Data de Nascimento"
+//           ></span>
+//         </div>
+
+//         <div className="container-login-form-btn">
+//           <button type="submit" className="login-form-btn">
+//             Register
+//           </button>
+//         </div>
+
+//         <div className="text-center">
+//           <span className="txt1">Já possui conta? </span>
+//           <Link className="txt2" to="/">
+//             Acessar com Email e Senha.
+//           </Link>
+//         </div>
+//         <div></div>
+//       </form>
+//     </LayoutComponents>
+//   );
+// };
+
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { LayoutComponents } from "../../components/LayoutComponents";
 import InputMask from "react-input-mask";
 
@@ -7,57 +235,61 @@ export const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
-  const [Cep, setCep] = useState("");
-  const [DataNascimento, setDataNascimento] = useState("");
+  const [cep, setCep] = useState("");
+  const [dataNascimento, setDataNascimento] = useState("");
+  const inputEmail = useRef(null);
+  const isFocused = inputEmail.current === document.activeElement;
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidCep, setIsValidCep] = useState(false);
 
-  function validateEmail(email) {
+  const validateEmail = (email) => {
     const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
-    if (email !== "" && email.match(regex)) {
-      setEmail(email);
-    }
-    return false;
-  }
+    return email !== "" && regex.test(email);
+  };
 
-  // useEffect(() => {
-  //   fetch('http://localhost:5000').then((data) => {
-  //     return data.json()
-  //   })
-  //     .then((data) => {
-  //       console.log(data)
-  //     })
-  // }, [])
+  const validateCep = (cep) => {
+    const regex = /^[0-9]{5}-?[0-9]{3}$/;
+    return cep !== "" && regex.test(cep);
+  };
 
   useEffect(() => {
-    fetch("http://localhost:5000")
-      .then((data) => data.json())
-      .then((data) => console.log(data));
-  }, []);
+    setIsValidEmail(validateEmail(email));
+    setIsValidCep(validateCep(cep));
+    console.log("focus", isFocused);
+  }, [email, cep, isFocused]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Additional registration logic
+  };
 
   return (
     <LayoutComponents>
-      <form className="login-form">
-        <span className="login-form-title"> Criar Conta </span>
-
-        <span className="login-form-title"></span>
+      <form action="submit" className="login-form" onSubmit={handleSubmit}>
+        <span className="login-form-title">Criar Conta</span>
 
         <div className="wrap-input">
           <input
             className={name !== "" ? "has-val input" : "input"}
-            type="email"
+            type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            required
           />
           <span className="focus-input" data-placeholder="Nome Completo"></span>
         </div>
 
         <div className="wrap-input">
           <input
+            ref={inputEmail}
             className={email !== "" ? "has-val input" : "input"}
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
           <span className="focus-input" data-placeholder="Email"></span>
+          {isFocused && !isValidEmail && <span>Email inválido</span>}
         </div>
 
         <div className="wrap-input">
@@ -69,25 +301,28 @@ export const Register = () => {
           />
           <span className="focus-input" data-placeholder="Password"></span>
         </div>
+
         <div className="wrap-input">
           <InputMask
             id="input-mask"
             mask="99999-999"
-            className={Cep !== "" ? "has-val input" : "input"}
-            value={Cep}
+            className={cep !== "" ? "has-val input" : "input"}
+            value={cep}
             onChange={(e) => setCep(e.target.value)}
           ></InputMask>
           <span
             className="focus-input"
-            data-placeholder="Digite seu Cep"
+            data-placeholder="Digite seu CEP"
           ></span>
+          {!isValidCep && <span>CEP inválido</span>}
         </div>
+
         <div className="wrap-input">
           <InputMask
             id="input-mask"
             mask="99/99/9999"
-            className={DataNascimento !== "" ? "has-val input" : "input"}
-            value={DataNascimento}
+            className={dataNascimento !== "" ? "has-val input" : "input"}
+            value={dataNascimento}
             onChange={(e) => setDataNascimento(e.target.value)}
           ></InputMask>
           <span
@@ -97,7 +332,9 @@ export const Register = () => {
         </div>
 
         <div className="container-login-form-btn">
-          <button className="login-form-btn">Register</button>
+          <button type="submit" className="login-form-btn">
+            Register
+          </button>
         </div>
 
         <div className="text-center">
@@ -106,7 +343,6 @@ export const Register = () => {
             Acessar com Email e Senha.
           </Link>
         </div>
-        <div></div>
       </form>
     </LayoutComponents>
   );
